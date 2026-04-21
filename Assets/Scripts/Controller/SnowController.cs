d552f3ce61bae677f9c238fe3cbb393b279bec30
gerstenbreinz <gerstenbreinz@gmail.com>
2026-04-21 11:57:21 +0200

    snow

---
+++ Assets/Scripts/Controller/SnowController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnowPlow.Controller;
+using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public class SnowController : MonoBehaviour
+    {
+        private SnowSystem snowSystem;
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+            if (snowSystem != null) snowSystem.Update(Time.deltaTime);
+        }
+
+        public void SetSnowSystem(SnowSystem system)
+        {
+            snowSystem = system;
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/SnowSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnowPlow.Model.Map;
+
+namespace SnowPlow.Controller
+{
+    public class SnowSystem
+    {
+        private List<Lane> lanes;
+
+        public float snowFallRate = 0.2f; // egység / másodperc
+
+        private float dt { get; set; } = 0;
+        public SnowSystem(List<Lane> lanes)
+        {
+            this.lanes = lanes;
+        }
+
+        public void Update(float deltaTime)
+        {
+            dt += deltaTime;
+
+            while (dt >= 5f)
+            {
+                dt -= 5f;
+
+                foreach (var lane in lanes)
+                {
+                    lane.AddSnow();
+                }
+            }
+
+
+        }
+    }
+}
